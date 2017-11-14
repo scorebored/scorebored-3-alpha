a2f1e534fae068c80b2a8f421d3949300afacb5a
Mike McGann <mike.mcgann@blackchip.org>
2017-11-14 17:37:32 -0500

    otherPlayer test

---
+++ src/util/player.test.js
@@ -0,0 +1,14 @@
+import {otherPlayer} from "@/util/player";
+
+describe("otherPlayer", () => {
+    test("otherPlayer(player0) is player1", () => {
+        expect(otherPlayer("player0")).toBe("player1");
+    });
+    test("otherPlayer(player1) is player0", () => {
+        expect(otherPlayer("player1")).toBe("player0");
+    });
+    test("otherPlayer(player2) is an error", () => {
+        expect(() => otherPlayer("player2")).toThrow();
+    });
+});
+
