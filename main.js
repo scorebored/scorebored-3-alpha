9e5bded22b8460c495cb7b82d86ea2f387af9994
Kenneth Barton <stappkb@gmail.com>
2017-11-14 17:59:04 -0500

    adding bootstrap

--- main.js
+++ main.js
@@ -15,8 +15,7 @@ if (isDevMode) {
 const createWindow = async () => {
   // Create the browser window.
   mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+      fullscreen: true
   });
 
   // and load the index.html of the app.
@@ -50,7 +49,7 @@ app.on('window-all-closed', () => {
       app.quit();
     }
   });
-  
+
   app.on('activate', () => {
     // On OS X it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
@@ -58,7 +57,6 @@ app.on('window-all-closed', () => {
       createWindow();
     }
   });
-  
+
   // In this file you can include the rest of your app's specific main process
   // code. You can also put them in separate files and import them here.
-  
--- src/components/MainMenu.js
+++ src/components/MainMenu.js
@@ -1,17 +1,63 @@
 import React from "react";
 import {Link} from "react-router-dom";
+import { Navbar, Jumbotron, Button } from 'react-bootstrap';
 
+var tableStyle = {
+    border: 'none'
+};
+
+var roundCorners = {
+    borderRadius: '10px'
+}
+
+let center_jacob = {
+    margin: '0 10px'
+}
 class MainMenu extends React.Component {
 
     render = () => {
         return (
-            <div>
-                <h3>Main Menu</h3>
-                <img src={`${__dirname}/../resources/paddle.png`}/>
-                <ul>
-                    <li><Link to="/start">Start Match</Link></li>
-                    <li><Link to="/excuses">Excuses</Link></li>
-                </ul>
+            <div className='center_shit'>
+                <div>
+                    <img src={`${__dirname}/../resources/S2_ping_pong.png`}/>
+                </div>
+                <table style={tableStyle}>
+                    <tbody>
+                        <tr>
+                            <td>
+                                <div className='form-horizontal give_me_space'>
+                                    <label className="control-label" htmlFor="startMatch">
+                                        Start Match
+                                    </label>
+                                    <div>
+                                        <Link id='startMatch' to="/start">
+                                            <img src={`${__dirname}/../resources/green_pong.png`}/>
+                                        </Link>
+                                    </div>
+                                </div>
+                            </td>
+                            <td>
+                                <div className='form-horizontal give_me_space'>
+                                    <label  style={center_jacob} className="control-label"
+                                    htmlFor="j_excuses">
+                                        Excuses
+                                    </label>
+                                    <div>
+                                        <Link id="j_excuses" to="/excuses">
+                                            <img style={roundCorners}
+                                            src={`${__dirname}/../resources/jacob.png`}/>
+                                        </Link>
+                                    </div>
+                                </div>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+                {/*
+                <Button bsSize="large" bsStyle="link">
+                    <Link to="/excuses">Excuses
+                    </Link>
+                </Button>*/}
             </div>
         );
     }
