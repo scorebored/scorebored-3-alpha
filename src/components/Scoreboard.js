7c05c33c350e174f3b3390288a59b4a25e6c6df4
Mike McGann <mike.mcgann@blackchip.org>
2017-11-22 11:34:10 -0500

    updates to style

--- src/components/Scoreboard.js
+++ src/components/Scoreboard.js
@@ -1,16 +1,17 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-const red = '#f00'
-const blue = '#00f'
-const off = '#444'
+const redTeamColor = '#f00'
+const blueTeamColor = '#00f'
+const onColor = '#fff'
+const matColor = '#222'
+const borderColor = '#aaa'
 
-const styleService = {
-    display: 'flex',
-    width: '40%',
-    justifyContent: 'center',
-    alignItems: 'center',
-    fontSize: '5vw',
+const styleServing = {
+    padding: '0 1vw',
+    border: '2px solid',
+    borderRadius: '15px',
+    borderColor: matColor,
 }
 
 const stylePoints = {
@@ -20,16 +21,17 @@ const stylePoints = {
     width: '40%',
     fontFamily: 'Montserrat Medium',
     fontSize: '35vw',
-    color: '#fff',
-    border: '20px solid #aaa',
+    color: onColor,
+    border: '20px solid',
+    borderColor,
     borderRadius: '15px',
 }
 
-const styleMatch = {
-    display: 'flex',
-    width: '40%',
-    alignItems: 'center',
-    fontSize: '5vw',
+const styleWins = {
+    border: '2px solid',
+    borderColor,
+    borderRadius: '15px',
+    padding: '0 1vw',
 }
 
 const styles = {
@@ -42,74 +44,84 @@ const styles = {
         display: 'flex',
         flexFlow: 'column',
         justifyContent: 'space-around',
-        backgroundColor: '#222',
+        backgroundColor: matColor,
     },
     serviceContainer: {
         display: 'flex',
         flexDirection: 'row',
         justifyContent: 'space-around',
-        height: '10%',
+        height: '11%',
     },
     pointsContainer: {
         display: 'flex',
         flexDirection: 'row',
         justifyContent: 'space-around',
-        height: '75%',
+        height: '68%',
     },
     matchContainer: {
         display: 'flex',
         flexDirection: 'row',
         justifyContent: 'space-around',
-        height: '10%',
+        height: '5%',
+    },
+    serviceItem: {
+        display: 'flex',
+        width: '40%',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: '5vw',
     },
     pointsRedOne: {
         ...stylePoints,
-        backgroundColor: red,
+        backgroundColor: redTeamColor,
         fontSize: '35vw',
     },
     pointsBlueOne: {
         ...stylePoints,
-        backgroundColor: blue,
+        backgroundColor: blueTeamColor,
         fontSize: '35vw',
     },
     pointsRedTwo: {
         ...stylePoints,
-        backgroundColor: red,
+        backgroundColor: redTeamColor,
         fontSize: '28vw',
     },
     pointsBlueTwo: {
         ...stylePoints,
-        backgroundColor: blue,
+        backgroundColor: blueTeamColor,
         fontSize: '28vw',
     },
     servingRed: {
-        ...styleService,
-        color: red,
+        ...styleServing,
+        borderColor,
+        backgroundColor: redTeamColor,
+        color: onColor,
     },
     servingBlue: {
-        ...styleService,
-        color: blue,
+        ...styleServing,
+        borderColor,
+        backgroundColor: blueTeamColor,
+        color: onColor,
     },
     servingOff: {
-        ...styleService,
-        color: off,
+        display: 'none',
     },
-    matchRed: {
-        ...styleMatch,
-        justifyContent: 'start',
-    },
-    matchBlue: {
-        ...styleMatch,
-        justifyContent: 'flex-end',
+    matchItem: {
+        display: 'flex',
+        width: '40%',
+        alignItems: 'center',
+        fontSize: '2vw',
+        justifyContent: 'center',
     },
     winRed: {
-        color: red,
+        ...styleWins,
+        backgroundColor: redTeamColor,
+        color: onColor,
     },
     winBlue: {
-        color: blue,
-    },
-    winOff: {
-        color: off,
+        ...styleWins,
+        backgroundColor: blueTeamColor,
+        color: onColor,
     }
 }
 
@@ -143,8 +155,12 @@ export default class Scoreboard extends React.Component {
         return (
             <div style={styles.main}>
                 <div style={styles.serviceContainer}>
-                    <div style={styleServiceRed}>🏓</div>
-                    <div style={styleServiceBlue}>🏓</div>
+                    <div style={styles.serviceItem}>
+                        <span style={styleServiceRed}>🏓</span>
+                    </div>
+                    <div style={styles.serviceItem}>
+                        <span style={styleServiceBlue}>🏓</span>
+                    </div>
                 </div>
                 <div style={styles.pointsContainer}>
                     <div style={stylePointsRed}>
@@ -155,10 +171,10 @@ export default class Scoreboard extends React.Component {
                     </div>
                 </div>
                 <div style={styles.matchContainer}>
-                    <div style={styles.matchRed}>
+                    <div style={styles.matchItem}>
                         {winsRed}
                     </div>
-                    <div style={styles.matchBlue}>
+                    <div style={styles.matchItem}>
                         {winsBlue}
                     </div>
                 </div>
@@ -167,26 +183,15 @@ export default class Scoreboard extends React.Component {
     }
 
     renderWins = (playerId) => {
-        const toWin = Math.ceil(this.props.matchLength / 2)
-        if (toWin === 1) {
+        if (this.props.matchLength === 1) {
             return null
         }
-        const colorOn = playerId === 0 ? styles.winRed : styles.winBlue
-        const components = []
-        for (let i = 1; i <= toWin; i++) {
-            let c = null
-            let key = `wins-${playerId}-${i}`
-            if (i <= this.props.wins[playerId]) {
-                c = <span key={key} style={colorOn}>★</span>
-            } else {
-                c = <span key={key} style={styles.winOff}>☆</span>
-            }
-            components.push(c)
+        const style = playerId === 0 ? styles.winRed : styles.winBlue
+        const stars = []
+        for (let i = 0; i < this.props.wins[playerId]; i++) {
+            stars.push('★')
         }
-        if (playerId === 1) {
-            components.reverse()
-        }
-        return components
+        return <span style={style}>{stars.join(' ')}</span>
     }
 
     keyPressed = (event) => {
